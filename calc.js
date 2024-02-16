a7ae376a3def7f6315bdf65a15d3304648bca8b8
JP7339 <ogeidiram11@gmail.com>
2024-02-15 22:31:16 -0700

    first commit

---
+++ calc.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const display = document.getElementById("screen");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = null;
+}
+
+function calculate(){
+    display.value = eval(display.value);
+}

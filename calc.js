a48cda59f1afe2fb0af8c38004ed372e106c37a9
JP7339 <ogeidiram11@gmail.com>
2024-03-12 19:51:43 -0600

    bug fixes / fixed calculator

--- calc.js
+++ calc.js
@@ -1,8 +1,17 @@
 
-const display = document.getElementById("screen");
+const display = document.getElementById('screen');
+let screen = "";
 
 function appendToDisplay(input){
+    console.log(input);
     display.value += input;
+    console.log(display);
+}
+s
+function subDisplay(){
+    screen = display.value;
+    screen = screen.substring(0,screen.length-1);
+    display.value = screen;
 }
 
 function clearDisplay(){
@@ -11,4 +20,5 @@ function clearDisplay(){
 
 function calculate(){
     display.value = eval(display.value);
+    console.log(display.value);
 }
